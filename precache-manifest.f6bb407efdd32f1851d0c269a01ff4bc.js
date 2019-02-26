7fff9358def79cee54b6b52ef047268d1f2f633b
cherylqian <cherylqian@outlook.com>
2019-02-25 20:20:16 -0600

    Updates

---
+++ precache-manifest.f6bb407efdd32f1851d0c269a01ff4bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/siggraph-website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/siggraph-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/siggraph-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/siggraph-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "537fea2527fe2249e9ac",
+    "url": "/siggraph-website/static/js/runtime~main.537fea25.js"
+  },
+  {
+    "revision": "9c15985f717fac7638e9",
+    "url": "/siggraph-website/static/js/main.9c15985f.chunk.js"
+  },
+  {
+    "revision": "d4d46e62a4ef9b9777de",
+    "url": "/siggraph-website/static/js/1.d4d46e62.chunk.js"
+  },
+  {
+    "revision": "9c15985f717fac7638e9",
+    "url": "/siggraph-website/static/css/main.e31d24d9.chunk.css"
+  },
+  {
+    "revision": "d4d46e62a4ef9b9777de",
+    "url": "/siggraph-website/static/css/1.1cd6806c.chunk.css"
+  },
+  {
+    "revision": "903fbf7e548aede09c91d7f942c2fccf",
+    "url": "/siggraph-website/index.html"
+  }
+];
