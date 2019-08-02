06f46ad67cc34744c03eca7b827b9dbe9bce8ff2
cherylqian <cherylqian@outlook.com>
2019-08-02 16:54:49 +0800

    Updates

---
+++ precache-manifest.9c7bbf91cd0aa399ea9f09eb90b4333c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15794426336841e36e03c2ba89069235",
+    "url": "/siggraph/index.html"
+  },
+  {
+    "revision": "12e5d355853dc60b3ab6",
+    "url": "/siggraph/static/css/main.e841216b.chunk.css"
+  },
+  {
+    "revision": "71ffc05bc8155a6e660d",
+    "url": "/siggraph/static/js/2.7011d81d.chunk.js"
+  },
+  {
+    "revision": "12e5d355853dc60b3ab6",
+    "url": "/siggraph/static/js/main.4eac2974.chunk.js"
+  },
+  {
+    "revision": "9193345ded18d91551f0",
+    "url": "/siggraph/static/js/runtime~main.8562dd1d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/siggraph/precache-manifest.adc24799f589c0af91379058960c974d.js"
+  "/siggraph/precache-manifest.9c7bbf91cd0aa399ea9f09eb90b4333c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/siggraph/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/siggraph/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
