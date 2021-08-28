e5059416a0abf2ddce3b5a464b5c0be6c8040e9c
smzeng <stacymz2@illinois.edu>
2021-08-28 11:57:02 -0500

    updated front page for hi tea

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import Background from "./Background";
 class App extends Component {
   render() {
     const { width, height } = this.props.size;
+    {/*
     return (
-      <div className="App" style={{backgroundImage: "url('high_Tea.png')"}}>
+      <div className="App">
         <Background width={width} height={height} />
         <h1 className="center w-100">SIGGRAPH @ UIUC</h1>
       </div>
+    );
+    */}
+    return (
+      <div style={{ height: '100%', position: 'absolute', left: '0px', width: '100%', overflow: 'hidden'}}>
+      <center>
+        <img className="poster" style ={{paddingbottom:'500px', height: '90%', width: '100%'}} src="/siggraph/assets/hi_tea.png" alt="hi tea poster" />
+        </center>
+      </div>
+
     );
   }
 }
